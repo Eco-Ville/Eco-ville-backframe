d1fbc607f839eb1831312fe6a85f2e9b688b3822
Emilio Kariuki <emilio113kariuki@gmail.com>
2022-12-20 10:36:56 +0300

    ADD added the review or the feedback router

---
+++ src/routes/feedback/(feedback).ts
@@ -0,0 +1,32 @@
+import { createHandler, z } from "@backframe/rest";
+import { prisma } from "../../db.js";
+
+export const POST = createHandler({
+    input: z.object({
+        feedback: z.string()
+    }),
+    async action(context){
+        const {feedback} = context.input;
+        try{
+            const review = await prisma.feedback.create({
+                data: {
+                    feedback: feedback
+                }
+            })
+            context.json({message: "Feedback submitted successfully", feedback: feedback})
+        }catch(e){
+            context.json({message: e.message})
+        }
+    }
+})
+
+export const GET = createHandler({
+    async action(context){
+        try{
+            const feedback = await prisma.feedback.findMany();
+            context.json({message: "Feedback fetched successfully", feedback: feedback})
+        }catch(e){
+            context.json({message: e.message})
+        }
+    }
+})
