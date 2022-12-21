ead5c2252f8b555f275a15c97bea6d9de5f10543
Emilio Kariuki <emilio113kariuki@gmail.com>
2022-12-21 20:57:31 +0300

    ADD added the put and the DELETE handler allowing a user to delete any post they wish or even update its details

--- src/routes/Post/$id.ts
+++ src/routes/Post/$id.ts
@@ -1,5 +1,6 @@
-import { InternalException, createHandler } from "@backframe/rest";
+import { InternalException, createHandler,z } from "@backframe/rest";
 import { prisma } from "../../db.js";
+import { protect } from "../../auth.js";
 
 export const GET = createHandler({
   async action(context) {
@@ -7,7 +8,7 @@ export const GET = createHandler({
       const id = context.params.id;
       const post = await prisma.post.findMany({
         where: {
-          type: id,
+          id: id,
         },
       });
       context.json({
@@ -18,4 +19,74 @@ export const GET = createHandler({
       context.json(InternalException(e.message));
     }
   },
+  middleware:[
+    protect
+  ]
 });
+
+
+export const PUT = createHandler({
+    input: z.object({
+        name: z.string(),
+        location: z.string(),
+        lon: z.number(),
+        lat: z.number(),
+        image: z.string(),
+        description: z.string(),
+        type: z.enum(["GLASS", "ORGANIC", "PLASTIC", "METAL", "ELECTRONIC"]),
+      }),
+    
+    async action(context){
+        const {name,location,lon,lat,image,description,type} = context.input;
+        try{
+            const id = context.params.id;
+            const post = await prisma.post.update({
+                data:{
+                    name:name,
+                    location:location,
+                    lon:lon,
+                    lat:lat,
+                    image: image,
+                    description:description,
+                    type:type
+                },
+                where:{
+                    id:id
+                }
+            })
+
+            context.json({
+                message: "Post updated",
+                post: post,
+            })
+        }catch(e){
+            context.json(InternalException(e.message));
+        }
+    },
+    middleware: [
+        protect
+    ]
+    
+})
+
+export const DELETE = createHandler({
+    async action(context){
+        try{
+            const id = context.params.id
+            const post = await prisma.post.delete({
+                where:{
+                    id: id
+                }
+            });
+            context.json({
+                message: "Post deleted",
+            })
+
+        }catch(e){
+            context.json(InternalException(e.message))
+        }
+    },
+    middleware:[
+        protect
+    ]
+})
